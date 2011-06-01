f0be2ba8250c503f476a024f3d4492c57a8f5e59
Vlad Velici <vlad.velici@gmail.com>
2011-06-02 01:55:22 +0300

    fixed config

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 // change the following paths if necessary
 $yii='/var/yii/framework/yii.php';
-$config=dirname(__FILE__).'../sipcore/config/main.php';
+$config=dirname(__FILE__).'/../sipcore/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
