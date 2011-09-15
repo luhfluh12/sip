4afa11c09179bd78925012803f8b5e06f4bd0ece
root <root@siponline.ro>
2011-09-15 03:32:45 +0300

    fdas

--- sipcore/yiic.php
+++ sipcore/yiic.php
@@ -1,11 +1,7 @@
 <?php
 
 // change the following paths if necessary
-<<<<<<< HEAD
 $yiic='/var/yii/framework/yiic.php';
-=======
-$yiic='/var/www/yii/framework/yiic.php';
->>>>>>> 263673a4bcb530198dcf6a71d27078074ec73f75
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
